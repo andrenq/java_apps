43864c51500fb35ae68a07eb4eb9a86c0bc95e3b
Andre Queiroz <andre.queiroz.jrvs.ca@gmail.com>
2019-07-03 17:35:09 +0000

    First JDBC commit

---
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,38 @@
+package ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCExecutor {
+    public static void main(String[] args) {
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport",
+                "postgres", "password");
+        try {
+            Connection connection = dcm.getConnection();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+
+            /*Customer customer = new Customer();
+            customer.setFirstName("George");
+            customer.setLastName("Washington");
+            customer.setEmail("george.washington@wh.gov");
+            customer.setPhone("(555)666-4321");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Mount Vernon");
+            customer.setState("VA");
+            customer.setZipCode("22121");
+            customerDAO.create(customer);*/
+
+            /*Customer customer =customerDAO.findById(10000);
+            System.out.println(customer);
+            customer.setEmail("gwashington@wh.gov");
+            customer = customerDAO.update(customer);
+            System.out.println(customer);*/
+            customerDAO.delete(10000);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
