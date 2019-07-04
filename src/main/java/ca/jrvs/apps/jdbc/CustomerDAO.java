f13bbfefebfe06db0770b56215ff7e375d2e6e35
Andre Queiroz <andre.queiroz.jrvs.ca@gmail.com>
2019-07-04 17:11:16 +0000

    adding the sql files

--- src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -7,17 +7,76 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAO extends DataAccessObject<Customer> {
-    private static final String INSERT  = "INSERT INTO customer (first_name, last_name,"
+    private static final String INSERT        = "INSERT INTO customer (first_name, last_name,"
             + " email, phone, address, city,state, zipcode) VALUES (?,?,?,?,?,?,?,?)";
-    private static final String GET_ONE = "SELECT customer_id,first_name, last_name,"
+    private static final String GET_ONE       = "SELECT customer_id,first_name, last_name,"
             + " email, phone, address, city,state, zipcode FROM customer where customer_id = ?";
-    private static final String UPDATE  = "UPDATE customer SET first_name=?, last_name=?, "
+    private static final String UPDATE        = "UPDATE customer SET first_name=?, last_name=?, "
             + " email=?, phone=?, address=?, city=?,state=?, zipcode=? "
             + "WHERE customer_id = ?";
-    private static final String DELETE  = "DELETE FROM customer WHERE customer_id = ?";
+    private static final String DELETE        = "DELETE FROM customer WHERE customer_id = ?";
+    private static final String GET_ALL_LMT   = "SELECT customer_id,first_name,last_name," +
+            "email,phone,address,city,state,zipcode FROM customer " +
+            "ORDER by last_name,first_name LIMIT ?";
+    private static final String GET_ALL_PAGED = "SELECT customer_id,first_name,last_name," +
+            "email,phone,address,city,state,zipcode FROM customer " +
+            "ORDER by last_name,first_name LIMIT ? OFFSET ?";
+
+    public List<Customer> findAllSorted(int limit) {
+        List<Customer> customers = new ArrayList<>();
+        try (PreparedStatement statement = this.connection.prepareStatement(GET_ALL_LMT)) {
+            statement.setInt(1, limit);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("customer_id"));
+                customer.setFirstName(resultSet.getString("first_name"));
+                customer.setLastName(resultSet.getString("last_name"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setPhone(resultSet.getString("phone"));
+                customer.setAddress(resultSet.getString("address"));
+                customer.setCity(resultSet.getString("city"));
+                customer.setState(resultSet.getString("state"));
+                customer.setZipCode(resultSet.getString("zipcode"));
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return customers;
+    }
+
+    public List<Customer> findAllSortedPaged(int limit, int pageNumber) {
+        List<Customer> customers = new ArrayList<>();
+        int offset = ((pageNumber - 1) * limit);
+        try (PreparedStatement statement = this.connection.prepareStatement(GET_ALL_PAGED)) {
+            statement.setInt(1, limit);
+            statement.setInt(2, offset);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("customer_id"));
+                customer.setFirstName(resultSet.getString("first_name"));
+                customer.setLastName(resultSet.getString("last_name"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setPhone(resultSet.getString("phone"));
+                customer.setAddress(resultSet.getString("address"));
+                customer.setCity(resultSet.getString("city"));
+                customer.setState(resultSet.getString("state"));
+                customer.setZipCode(resultSet.getString("zipcode"));
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return customers;
+    }
 
     public CustomerDAO(Connection connection) {
         super(connection);
@@ -56,6 +115,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     @Override
     public Customer update(Customer dto) {
         Customer customer = null;
+        try {
+            this.connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         try (PreparedStatement statement = this.connection.prepareStatement(UPDATE)) {
             statement.setString(1, dto.getFirstName());
             statement.setString(2, dto.getLastName());
@@ -67,8 +132,15 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             statement.setString(8, dto.getZipCode());
             statement.setLong(9, dto.getId());
             statement.execute();
+            this.connection.commit();
             customer = this.findById(dto.getId());
         } catch (SQLException e) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException ex) {
+                e.printStackTrace();
+                throw new RuntimeException(ex);
+            }
             e.printStackTrace();
             throw new RuntimeException(e);
         }
--- src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,17 +1,36 @@
 package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.SQLInvalidAuthorizationSpecException;
+import java.util.List;
 
 public class JDBCExecutor {
-    public static void main(String[] args) {
-        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "hplussport",
-                "postgres", args[0]); //args[0] = your password
+    public static void main(String[] args) throws SQLInvalidAuthorizationSpecException {
+        String pgPassword = System.getenv("PGPASSWORD");
+        String pgUser = System.getenv("PGUSER");
+        String pgHost = System.getenv("PGHOST");
+        String pgDatabase = "hplussport";
+        if ((pgPassword == null) ||
+                (pgHost == null) ||
+                (pgUser == null)) {
+            try {
+                pgHost = args[0];
+                pgUser = args[1];
+                pgPassword = args[2];
+            } catch (Exception e) {
+                throw new SQLInvalidAuthorizationSpecException("\nSet database credentials as environmental variables:\n" +
+                        "\tPGHOST,PGUSER,PGPASSWORD\n " +
+                        "or send as arguments:\n" +
+                        "\targ[0]=PGHOST, arg[1]=PGUSER, arg[2]=PGPASSWORD");
+            }
+        }
+
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager(pgHost, pgDatabase,
+                pgUser, pgPassword);
         try {
             Connection connection = dcm.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
+            OrderDAO orderDAO = new OrderDAO(connection);
 
             /*Customer customer = new Customer();
             customer.setFirstName("George");
@@ -29,7 +48,24 @@ public class JDBCExecutor {
             customer.setEmail("gwashington@wh.gov");
             customer = customerDAO.update(customer);
             System.out.println(customer);*/
-            customerDAO.delete(10000);
+
+            /*Order order;
+            order = orderDAO.findById(1011);
+            System.out.println(order);*/
+
+            /*List<Order> orders = orderDAO.getOrdersForCustomer(789);
+            orders.forEach(System.out::println);*/
+
+            /*customerDAO.findAllSorted(20).forEach(System.out::println);*/
+
+            int n = 4;
+            System.out.println("Paged");
+            for (int i = 1; i < n + 1; i++) {
+                System.out.println("=============> Page " + i);
+                customerDAO.findAllSortedPaged(4, n).forEach(System.out::println);
+            }
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
