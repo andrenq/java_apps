2898d9711e9625cea7246493007096e61e53a0f8
Andre Queiroz <andre.queiroz.jrvs.ca@gmail.com>
2019-07-05 15:44:51 +0000

    Creating twitter app

---
+++ src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
@@ -0,0 +1,17 @@
+package ca.jrvs.apps.twitter.example;
+
+import java.util.Arrays;
+//import
+
+public class TwitterApiTest {
+    private static String CONSUMER_KEY="";
+    private static String CONSUMER_SECRET="";
+    private static String ACCESS_TOKEN="";
+    private static String TOKEN_SECRET="";
+
+    public static void main(String[] args) throws Exception {
+//        OAuthConsumer consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,CONSUMER_SECRET);
+    }
+
+
+}
